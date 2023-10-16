0761911a389c133854b42d6405778783dae8f4b7
Kirill Shaplyko <Baltazore@users.noreply.github.com>
2023-10-16 13:44:38 +0200

    Add handler example for create/update

---
+++ snippets/updating-data/create-record/handler.js
@@ -0,0 +1,50 @@
+// Create handler for serialization of any record
+import { recordIdentifierFor } from '@ember-data/store';
+import { serializeResources } from '@ember-data/json-api/request';
+
+const updatesHandler = {
+  MUTATION_OPS = new Set(['createRecord', 'updateRecord']);
+  request(context, next) {
+    if (!MUTATION_OPS.has(context.request.op)) {
+      // Not a mutation, do nothing
+      return next(context.request);
+    }
+
+    if (context.request.body) {
+      // body is already set, do nothing
+      return next(context.request);
+    }
+
+    const { data, store } = context.request;
+    const newRequestParams = Object.assign({}, context.request, {
+      body: serializeResources(
+        store.cache,
+        recordIdentifierFor(data.record)
+      )
+    });
+    return next(newRequestParams);
+  }
+}
+
+// Add this handler to your request manager
+export default class extends RequestManager {
+  constructor(args) {
+    super(args);
+    this.use([updatesHandler, Fetch]);
+    this.useCache(CacheHandler);
+  }
+}
+
+// then in your app just use createRecord builder and let handler care about serialization
+import { createRecord } from '@ember-data/json-api/request';
+
+const request = createRecord(record);
+await this.store.request(request);
+
+// or overwrite body if you need to, handler will not touch it
+import { createRecord } from '@ember-data/json-api/request';
+
+const record = this.store.createRecord('feature', { name: "rest-enabled" });
+const request = createRecord(record);
+// For some reason your endpoint for 'features' is not JSON:API compliant
+request.body = JSON.stringify({ name: 'rest-enabled' })
