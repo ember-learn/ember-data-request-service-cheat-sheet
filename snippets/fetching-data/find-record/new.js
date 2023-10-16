efcfecd11e561237eb12a83197300daaf6af49f0
Kirill Shaplyko <Baltazore@users.noreply.github.com>
2023-10-16 13:42:36 +0200

    Fix review comments from Chris

--- snippets/fetching-data/find-record/new.js
+++ snippets/fetching-data/find-record/new.js
@@ -1,3 +1,4 @@
 import { findRecord } from '@ember-data/json-api/request';
 
-const { content: { data: user } } = await this.store.request(findRecord('user', '1'));
+const result = await this.store.request(findRecord('user', '1'));
+const user = result.content.data
