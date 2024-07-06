40a2e26bf417e9ff185f1508b503049d3f7d29ea
Kirill Shaplyko <Baltazore@users.noreply.github.com>
2024-07-06 17:31:32 +0200

    Update snippets/fetching-data/find-record/own-builder.ts
    Co-authored-by: Ignace Maes <ignacemaes100@msn.com>

--- snippets/fetching-data/find-record/own-builder.ts
+++ snippets/fetching-data/find-record/own-builder.ts
@@ -4,7 +4,7 @@ import { pluralize } from 'ember-inflector';
 import type { RequestSignature } from '@warp-drive/core-types/symbols';
 import type { TypeFromInstance } from '@warp-drive/core-types/record';
 import type { FindRecordOptions } from '@warp-drive/core-types/request';
-import type { UrlOptions } from '@ember-data/request-utils'
+import type { UrlOptions } from '@ember-data/request-utils';
 
 type MyRequest<Type> = {
   url: string
