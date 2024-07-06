4af61eae83274f6170071fa8944a5e28baa71e39
Kirill Shaplyko <Baltazore@users.noreply.github.com>
2024-07-06 16:19:29 +0200

    Add types to options object

--- snippets/fetching-data/find-record/own-builder.ts
+++ snippets/fetching-data/find-record/own-builder.ts
@@ -4,6 +4,7 @@ import { pluralize } from 'ember-inflector';
 import type { RequestSignature } from '@warp-drive/core-types/symbols';
 import type { TypeFromInstance } from '@warp-drive/core-types/record';
 import type { FindRecordOptions } from '@warp-drive/core-types/request';
+import type { UrlOptions } from '@ember-data/request-utils'
 
 type MyRequest<Type> = {
   url: string
@@ -17,7 +18,7 @@ type MyRequest<Type> = {
 function findRecord<Type>(type: TypeFromInstance<Type>, id: string, options: FindRecordOptions<Type>): MyRequest<Type> {
   const identifier = { type, id };
 
-  const urlOptions = {
+  const urlOptions: Partial<UrlOptions> = {
     op: 'findRecord',
     identifier,
     resourcePath: pluralize(identifier.type),
